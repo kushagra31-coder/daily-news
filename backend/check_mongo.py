c07381c39465e7fc50abacd62299a03086498481
kushagra singh tomar <kushagrasingh240033@acropolis.in>
2026-09-22 13:10:17 +0530

    chore: remove backend/check_mongo.py

--- backend/check_mongo.py
+++
@@ -1,8 +0,0 @@
-﻿import asyncio
-from database import get_database
-async def check():
-    db = get_database()
-    col = db['articles']
-    doc = await col.find_one()
-    print(doc)
-asyncio.run(check())
