3c8e5aef4a47751222bf5d11c187efed008cccdb
kushagra singh tomar <kushagrasingh240033@acropolis.in>
2026-09-22 13:10:33 +0530

    chore: remove fix-env.js

--- fix-env.js
+++
@@ -1,8 +0,0 @@
-﻿const fs = require('fs');
-let index = fs.readFileSync('apps/worker/src/index.ts', 'utf-8');
-if (!index.includes('TURSO_URL')) {
-    index = index.replace(/export type Bindings = \{[\s\S]*?\}/, (match) => {
-        return match.replace('}', '  TURSO_URL?: string;\n  TURSO_AUTH_TOKEN?: string;\n}');
-    });
-    fs.writeFileSync('apps/worker/src/index.ts', index);
-}
