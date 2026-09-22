6a7f983e200d8122618060794ddf001122c24b2d
kushagra singh tomar <kushagrasingh240033@acropolis.in>
2026-09-22 13:10:41 +0530

    chore: remove fix.js

--- fix.js
+++
@@ -1,5 +0,0 @@
-﻿const fs = require('fs');
-let code = fs.readFileSync('frontend/components/BreakingBanner.tsx', 'utf-8');
-code = code.replace(/  \/\/ Only show if article is less than 2 hours old\n  const ageHours = differenceInHours\(new Date\(\), parseISO\(publishedAt\)\)\n  if \(ageHours >= 2\) return null/, '');
-code = code.replace(/import \{ differenceInHours, parseISO \} from 'date-fns'/, '');
-fs.writeFileSync('frontend/components/BreakingBanner.tsx', code, 'utf-8');
