253cdbf356b7dc6355b85a5bbd87d6604c9fbec2
kushagra singh tomar <kushagrasingh240033@acropolis.in>
2026-09-22 13:11:00 +0530

    chore: remove patch_main.py

--- patch_main.py
+++
@@ -1,6 +0,0 @@
-﻿import re
-with open('backend/main.py', 'r', encoding='utf-8') as f:
-    code = f.read()
-code = re.sub(r',\s*dependencies=\[Depends\(RateLimiter\(.*?\)\]', '', code, flags=re.DOTALL)
-with open('backend/main.py', 'w', encoding='utf-8') as f:
-    f.write(code)
