cf5767ebc335e0dfcdd48da02ededa7ad08e4660
kushagra singh tomar <kushagrasingh240033@acropolis.in>
2026-09-22 13:10:43 +0530

    chore: remove fix_cache.py

--- fix_cache.py
+++
@@ -1,8 +0,0 @@
-﻿import re
-with open('backend/main.py', 'r', encoding='utf-8') as f:
-    code = f.read()
-
-code = re.sub(r'    await cache_response\(cache_key,.*?\)\n', '', code)
-
-with open('backend/main.py', 'w', encoding='utf-8') as f:
-    f.write(code)
